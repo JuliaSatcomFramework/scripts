a0f6c586aeb467bd21c7ca832f4c7a54dc83610f
Alberto Mengali <disberd@gmail.com>
2025-06-01 18:05:12 +0200

    add script for overriding LibCURL

---
+++ override_libcurl.jl
@@ -0,0 +1,30 @@
+# This is a script to work around the issue in https://discourse.julialang.org/t/downloads-download-not-working-on-github-while-os-curl-does/129272
+import LibCURL_jll
+
+# We only do something if LibCURL_jll does not have OpenSSL
+if pkgversion(LibCURL_jll) < v"8.8"
+    sys_curl_path = read(`which curl`, String) |> strip
+    sys_libcurl_path = let
+        out = read(pipeline(`ldd $sys_curl_path`, `grep libcurl`), String) |> strip
+        m = match(r"=> ([^\s]+) \(0x[0-9a-f]+\)", out)
+        m.captures[1]
+    end
+
+    julia_libcurl_name = LibCURL_jll.libcurl
+    if julia_libcurl_name != sys_libcurl_path
+        @info "Overwriting the LibCURL_jll.jl file to point to the system libcurl" julia_libcurl_name sys_libcurl_path
+        julia_libcurljll_path = pkgdir(LibCURL_jll)
+        entrypoint = joinpath(julia_libcurljll_path, "src", "LibCURL_jll.jl")
+        newio = IOBuffer()
+        for line in eachline(entrypoint)
+            if !isnothing(findfirst(julia_libcurl_name, line))
+                newline = replace(line, julia_libcurl_name => sys_libcurl_path)
+                newline *= " # Line modified to use system libcurl"
+                println(newio, newline)
+            else
+                println(newio, line)
+            end
+        end
+        write(entrypoint, take!(newio))
+    end
+end
